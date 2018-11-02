41874ad3cfa70a513789129f11d7d5015be982c3
CodingLife <44698989+One-One-Code@users.noreply.github.com>
2018-11-02 18:33:12 +0800

    初始化

---
+++ Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace CvTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "图片处理";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 64);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(319, 258);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(360, 64);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(319, 258);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(13, 349);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(319, 258);
+            this.pictureBox3.TabIndex = 1;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(360, 349);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(319, 258);
+            this.pictureBox4.TabIndex = 1;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(726, 64);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(319, 258);
+            this.pictureBox5.TabIndex = 1;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(726, 349);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(319, 258);
+            this.pictureBox6.TabIndex = 1;
+            this.pictureBox6.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(112, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "数字识别";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(945, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(289, 15);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "80";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(245, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "阈值";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1189, 671);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CvTest
+{
+    using System.Diagnostics;
+    using System.Drawing.Imaging;
+    using System.Runtime.InteropServices;
+
+    using Emgu.CV;
+    using Emgu.CV.CvEnum;
+    using Emgu.CV.OCR;
+    using Emgu.CV.Structure;
+    using ZXing;
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var image = new Image<Bgr, byte>(@"E:\测试图片\2018-09-05_133455.png");
+            this.pictureBox1.Image = image.ToBitmap();
+            var grayimage = image.Convert<Gray, byte>();
+            this.pictureBox2.Image = grayimage.ToBitmap();
+            
+           
+            //var threshImage = grayimage.ThresholdAdaptive(new Gray(255), AdaptiveThresholdType.MeanC, ThresholdType.Binary, 9, new Gray(5));
+            //CvInvoke.NamedWindow("ThresholdAdaptive");
+            //CvInvoke.Imshow("ThresholdAdaptive", threshImage);
+            //CvInvoke.Threshold(grayimage, threshimg, 0, 255, ThresholdType.Otsu);
+
+            //CvInvoke.NamedWindow("Otsu");
+            //CvInvoke.Imshow("Otsu", threshImage);
+            //CvInvoke.Line(image, new Point(10, 10), new Point(150, 150), new MCvScalar(211,203,0));
+            //CvInvoke.NamedWindow("line");
+            //CvInvoke.Imshow("line", image);
+
+            //均衡化
+            Image<Gray, Byte> histimg = new Image<Gray, Byte>(grayimage.Width, grayimage.Height, new Gray(0.1));
+            CvInvoke.EqualizeHist(grayimage, histimg);
+            MIplImage histmi = (MIplImage)Marshal.PtrToStructure(histimg, typeof(MIplImage));
+            Image<Gray, Byte> histimage = new Image<Gray, Byte>(histimg.Width, histimg.Height, histmi.WidthStep, histmi.ImageData);
+            this.pictureBox5.Image = histimage.ToBitmap();
+
+            //二值化
+            Image<Gray, Byte> threshimg = new Image<Gray, Byte>(grayimage.Width, grayimage.Height);
+            CvInvoke.Threshold(histimg, threshimg, 120, 255, ThresholdType.Binary);
+            this.pictureBox3.Image = threshimg.ToBitmap();
+            //BarcodeReader reader = new BarcodeReader();
+            //reader.Options.CharacterSet = "utf-8";
+            //var result = reader.Decode(threshimg.ToBitmap());
+            //if (result != null)
+            //{
+            //    this.textBox1.Text = result.Text;
+            //}
+
+
+            //canny算子边缘检测
+            Image<Gray, Byte> cannyimg = new Image<Gray, Byte>(grayimage.Width, grayimage.Height, new Gray(0.1));
+            CvInvoke.Canny(threshimg, cannyimg, 10, 30);
+            MIplImage cannymi = (MIplImage)Marshal.PtrToStructure(cannyimg, typeof(MIplImage));
+            Image<Gray, Byte> cannyimage = new Image<Gray, Byte>(cannymi.Width, cannymi.Height, cannymi.WidthStep, cannymi.ImageData);
+            this.pictureBox4.Image = cannyimage.ToBitmap();
+
+
+            //circle画圈
+            Image<Gray, Byte> circleimg = new Image<Gray, Byte>(grayimage.Width, grayimage.Height, new Gray(255));
+            CvInvoke.Circle(circleimg, new Point(150, 150), 50, new MCvScalar(100, 100, 255));
+            this.pictureBox6.Image = circleimg.ToBitmap();
+
+
+            ////mix
+            //Image<Gray, Byte> miximg = new Image<Gray, Byte>(grayimage.Width, grayimage.Height, new Gray(255));
+            //Image<Gray, Byte> miximg1 = new Image<Gray, Byte>(grayimage.Width, grayimage.Height, new Gray(0.1));
+            //Image<Gray, Byte> miximg2 = new Image<Gray, Byte>(grayimage.Width, grayimage.Height, new Gray(255));
+            //CvInvoke.ColorChange(miximg1, miximg2, miximg);
+            //this.pictureBox6.Image = img_gray(miximg.Bitmap);
+        }
+
+        /// <summary>
+        /// 数字识别按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var sold = Convert.ToInt32(this.textBox2.Text);
+            var image = new Image<Bgr, byte>("9502.jpg");
+            this.pictureBox1.Image = image.ToBitmap();
+            Image<Bgr, Byte> threshimg = new Image<Bgr, Byte>(image.Width, image.Height);
+            CvInvoke.Threshold(image, threshimg, sold, 255, ThresholdType.Binary);
+            Image<Bgr, Byte> ercodeimg = new Image<Bgr, Byte>(image.Width, image.Height);
+            var element = CvInvoke.GetStructuringElement(ElementShape.Cross, new Size(1, 1), new Point(-1, -1));
+            CvInvoke.Erode(threshimg, ercodeimg, element, new Point(-1, -1), 1, BorderType.Reflect, default(MCvScalar));
+            this.pictureBox2.Image = ercodeimg.ToBitmap();
+            Tesseract ocr = new Tesseract("", "chi_sim", OcrEngineMode.TesseractOnly);
+            ocr.SetImage(ercodeimg);
+            ocr.SetVariable("tessedit_char_whitelist", "01234567890");
+            var result = ocr.GetUTF8Text();
+            this.textBox1.Text = result;
+        }
+
+        /// <summary>
+        /// 自己写的灰度化方法，用于性能比较
+        /// </summary>
+        /// <param name="curBitmap"></param>
+        /// <returns></returns>
+        public static unsafe Bitmap img_gray(Bitmap curBitmap)
+        {
+            int width = curBitmap.Width;
+            int height = curBitmap.Height;
+            Bitmap back = new Bitmap(width, height);
+            byte temp;
+            Rectangle rect = new Rectangle(0, 0, curBitmap.Width, curBitmap.Height);
+            //这种速度最快
+            BitmapData bmpData = curBitmap.LockBits(rect, ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);//24位rgb显示一个像素，即一个像素点3个字节，每个字节是BGR分量。Format32bppRgb是用4个字节表示一个像素
+            byte* ptr = (byte*)(bmpData.Scan0);
+            for (int j = 0; j < height; j++)
+            {
+                for (int i = 0; i < width; i++)
+                {
+                    //ptr[2]为r值，ptr[1]为g值，ptr[0]为b值
+                    temp = (byte)(0.299 * ptr[2] + 0.587 * ptr[1] + 0.114 * ptr[0]);
+                    back.SetPixel(i, j, Color.FromArgb(temp, temp, temp));
+                    ptr += 3; //Format24bppRgb格式每个像素占3字节
+                }
+                ptr += bmpData.Stride - bmpData.Width * 3;//每行读取到最后“有用”数据时，跳过未使用空间XX
+            }
+            curBitmap.UnlockBits(bmpData);
+            return back;
+        }
+    }
+}
